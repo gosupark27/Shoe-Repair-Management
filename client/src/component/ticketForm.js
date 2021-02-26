698cef90bed3b92ba9839ec338ed52a56d91deb3
gosupark <hpark41@wgu.edu>
2021-02-25 23:20:40 -0700

    testing...

--- client/src/component/ticketForm.js
+++ client/src/component/ticketForm.js
@@ -1,19 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
-const TicketForm = () =>{
+
+const TicketForm = () => {
     //useState
-    const [firstName, setFirstName] = useState(" ")
-    const handleChange = (event) => {
-        setFirstName(event.target.value);
-        console.log(event.target.value);
-      };
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [pickUpDate, setPickUpDate] = useState('')
+    const [ticketNumber, setTicketNumber] = useState('')
+    const [dropDate, setDropDate] = useState('')
+    // const handleChange = (event) => {
+    //     setFirstName(event.target.value);
+    //     console.log(event.target);
+    //   };
 
-  
-  return(
-    <TextField id="outlined-basic" label="First Name" variant="outlined" value={firstName} onChange={handleChange} />
-  )
+
+    return (
+        <Grid container>
+            <Grid container>
+                <Grid item xs={12} >
+                    <TextField id="outlined-basic" label="Ticket Number" variant="outlined" value={ticketNumber} onChange={(e) => setTicketNumber(e.target.value)} />
+                    <TextField id="outlined-basic" label="Drop Date" variant="outlined" value={dropDate} onChange={(e) => setDropDate(e.target.value)} />
+
+                </Grid>
+            </Grid>
+            <Grid container>
+
+                <Grid item xs={12}>
+                    <TextField id="outlined-basic" label="First Name" variant="outlined" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+
+                    <TextField id="outlined-basic" label="Last Name" variant="outlined" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                </Grid>
+
+            </Grid>
+            <Grid container>
+
+                <Grid item xs={12}>
+                    <TextField id="outlined-basic" label="Phone" variant="outlined" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                    <TextField id="outlined-basic" label="Pickup Date" variant="outlined" value={pickUpDate} onChange={(e) => setPickUpDate(e.target.value)} />
+                </Grid>
+
+            </Grid>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary">
+                        Create Tickets
+            </Button>
+                </Grid>
+            </Grid>
+
+        </Grid>
+    )
 
 }
 
